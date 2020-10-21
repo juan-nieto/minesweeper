b91f317022c63ff663bf205439c77ee0b01c5ab1
juan-nieto <nieto.juan15@gmail.com>
2020-10-21 11:14:20 -0600

    Updates

---
+++ precache-manifest.3cb39e6cfe571131f937dfad3e4fd413.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fb3286558027d227581a3749a302b6",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "d5df4d84c38c28874fc4",
+    "url": "/minesweeper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eea2b631c125b1aaa82c",
+    "url": "/minesweeper/static/js/2.c17c213e.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/minesweeper/static/js/2.c17c213e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5df4d84c38c28874fc4",
+    "url": "/minesweeper/static/js/main.727918ec.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/minesweeper/static/media/logo.5d5d9eef.svg"
+  }
+]);
